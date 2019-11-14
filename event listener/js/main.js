6040faeed3eee1a4bc2df033072cb54a98679b5f
donya1380 <56601701+donya1380@users.noreply.github.com>
2019-11-14 11:28:25 -0800

    Add files via upload

---
+++ event listener/js/main.js
@@ -0,0 +1,41 @@
+function changecolor() {
+
+	var x =document.getElementById("change");
+	var y = x.getElementsByClassName("child");
+	var i;
+	for ( i = 0; i < y.lenght; i++) {
+		y[i].style.backgroundColor = "green";
+	}
+}
+
+function color(){
+	
+	var x = document.getElementById("change");
+	var y = x.getElementsByClassName("child");
+	var i;
+	for(i = 0; i < y.lenght; i ++){
+		y[i].style.color = 'green';
+	}
+}
+
+	function myfunction2(){
+		var list = document.getElementsByTagName("ul")[0];
+		list.getElementsByTagName("li")[0].innerHTML = "cake";
+	}
+
+function myfunction3(){
+		var gg = document.getElementsByTagName("ul")[0];
+		gg.getElementsByTagName("li")[1].innerHTML = "pastery";
+	}
+
+	function myfunction1() {
+	var person = prompt("please enter your name","harry potter");
+	if (person != null) {
+		document.getElementById("insert").innerHTML = 
+		"hello " + person + "!how are you today?";
+	}
+}
+
+function insert(){
+	document.writeln("<h1>Welcome to Javascript Programming!</h1>");
+}
---
+++ helloworld/js/main.js
@@ -0,0 +1,19 @@
+// alert("hello,I'm internal");
+// console.log(" Hi there!");
+function age(){
+let age = prompt(" hello,"+ age +" how old are you?");
+	alert("you are" + age + "years old")
+}
+// let number = parseInt(prompt("give me a number"),10);
+// let total = number + 10;
+// alert(total);
+
+function name(){
+let name = prompt("hello, what is your name?");
+ document.getElementById("demo").innerHTML = "hello " + name;
+ alert(name);
+}     
+
+function hello(){
+	alert("hello, world");
+}
